6d223036dba044614534e0763a5dcbe425adc362
Vincent Bengtsson <vinben9928@edu.halmstad.se>
2018-10-10 08:34:48 +0200

    Created Firebase chat.

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,40 @@
 window.addEventListener("DOMContentLoaded", documentLoaded);
 
+var messages = firebase.database().ref("chat");
+
+messages.on("value", function(data) {
+    var msgs = data.val();
+
+    var chatMessages = document.getElementById("chatMessages");
+    while(chatMessages.firstChild) {
+        chatMessages.removeChild(chatMessages.firstChild);
+    }
+
+    for(var key in msgs) {
+        var message = msgs[key];
+        
+        var msgElement = document.createElement("div");
+        var headerElement = document.createElement("p");
+        var messageElement = document.createElement("p");
+
+        headerElement.innerText = new Date(message.timestamp).toString() + " - " + message.name;
+        headerElement.style.cssText = "font-weight: bold;";
+
+        messageElement.innerText = message.message;
+
+        msgElement.style.cssText = "padding: 16px; border-bottom: 1px dashed #CCCCCC;";
+
+        msgElement.appendChild(headerElement);
+        msgElement.appendChild(messageElement);
+        
+        chatMessages.appendChild(msgElement);
+    }
+
+    if(chatMessages.children.length > 0) {
+        chatMessages.children[chatMessages.children.length - 1].scrollIntoView();
+    }
+});
+
 function documentLoaded() {
     document.getElementById("chatForm").addEventListener("submit", onSubmit)
 }
@@ -11,7 +46,15 @@ function onSubmit(event) {
     var message = document.getElementById("chatMessage").value;
     var timestamp = Date.now();
 
-    
+    let database = firebase.database();
+
+    database.ref("chat").push({
+        name: name,
+        message: message,
+        timestamp: timestamp
+    });
+
+    document.getElementById("chatMessage").value = "";
 }
 
 function getElementsByClassName(className) {
